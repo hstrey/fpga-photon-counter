c172d9013a8e693507e289cf9e8745cf9d540096
Helmut Strey <Helmut.Strey@gmail.com>
2020-06-30 00:01:09 -0400

    started with actual photon flux measurement

---
+++ photorate/work/project.tcl
@@ -0,0 +1,17 @@
+set projDir "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/vivado"
+set projName "photorate"
+set topName top
+set device xc7a35tftg256-1
+if {[file exists "$projDir/$projName"]} { file delete -force "$projDir/$projName" }
+create_project $projName "$projDir/$projName" -part $device
+set_property design_mode RTL [get_filesets sources_1]
+set verilogSources [list "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/verilog/au_top_0.v" "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/verilog/reset_conditioner_1.v" "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/verilog/multi_seven_seg_2.v" "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/verilog/counter_3.v" "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/verilog/bin_to_dec_4.v" "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/verilog/counter_5.v" "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/verilog/seven_seg_6.v" "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/verilog/decoder_7.v" ]
+import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
+set xdcSources [list "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/constraint/io.xdc" "/home/streylab/Documents/programming/fpga-photon-counter/photorate/work/constraint/alchitry.xdc" "/home/streylab/Desktop/fpga/alchitry-labs-1.1.5/library/components/au.xdc" ]
+read_xdc $xdcSources
+set_property STEPS.WRITE_BITSTREAM.ARGS.BIN_FILE true [get_runs impl_1]
+update_compile_order -fileset sources_1
+launch_runs -runs synth_1 -jobs 8
+wait_on_run synth_1
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
